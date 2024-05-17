27fac834fae6ddad3c141e62741e7feb890070b8
dekolor <stefanbriceag1@gmail.com>
2024-05-17 15:34:18 +0300

    Added mutation to store a 'file' in db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,11 @@ import {
   SignedOut,
   useSession,
 } from "@clerk/nextjs";
-import Image from "next/image";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const session = useSession();
+  const createFile = useMutation(api.files.createFile);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -25,6 +26,16 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      <Button
+        onClick={() => {
+          createFile({
+            name: "hello world",
+          });
+        }}
+      >
+        Click Me
+      </Button>
     </main>
   );
 }
