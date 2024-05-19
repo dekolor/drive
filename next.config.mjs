8ebf0ec44b2c39bb0259a00dcfc2c2a7983bc09c
dekolor <stefanbriceag1@gmail.com>
2024-05-19 11:46:54 +0300

    added image preview for files + download functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "https://fast-sparrow-58.convex.cloud",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import UploadButton from "./upload-button";
 import { FileCard } from "./file-card";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   const organization = useOrganization();
@@ -16,18 +18,43 @@ export default function Home() {
     orgId = organization.organization?.id ?? user.user?.id;
   }
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
+  const isLoading = files === undefined;
 
   return (
     <main className="container mx-auto pt-12">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl fontbold">Your Files</h1>
-        <UploadButton />
-      </div>
-      <div className="grid grid-cols-4 gap-4">
-        {files?.map((file) => {
-          return <FileCard key={file._id} file={file} />;
-        })}
-      </div>
+      {isLoading && (
+        <div className="flex flex-col gap-8 w-full items-center mt-12">
+          <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
+          <div className="text-2xl">Loading your files...</div>
+        </div>
+      )}
+
+      {!isLoading && files.length === 0 && (
+        <div className="flex flex-col gap-8 w-full items-center mt-12">
+          <Image
+            alt="an image of a picture and directory icon"
+            width="300"
+            height="300"
+            src="/empty.svg"
+          />
+          <div className="text-2xl ">You have no files, upload one now</div>
+          <UploadButton />
+        </div>
+      )}
+
+      {!isLoading && files.length > 0 && (
+        <>
+          <div className="flex justify-between items-center mb-8">
+            <h1 className="text-4xl fontbold">Your Files</h1>
+            <UploadButton />
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            {files?.map((file) => {
+              return <FileCard key={file._id} file={file} />;
+            })}
+          </div>
+        </>
+      )}
     </main>
   );
 }
